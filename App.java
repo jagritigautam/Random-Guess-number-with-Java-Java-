e8343516ebf431e1b50da71b3f4e5919cda4ea90
jagritigautam <76871120+jagritigautam@users.noreply.github.com>
2024-06-23 11:24:02 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) {
+        scanner sc = newscanner(System.in);
+        int a = sc.nextint();
+        int b = sc.nextint ();
+        int Sum = a + b; 
+        System.out.println(Sum);
+    }
+    
+}
